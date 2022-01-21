25982d106d19191a00c23ab076439627a1c32982
pabpereza <pabloperezaradros@gmail.com>
2022-01-21 14:50:28 +0100

    Actualizado hugo

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 rm -rf public/
 git pull
-HUGO_ENV="production" /snap/bin/hugo --gc || exit 1
+HUGO_ENV="production" /usr/local/bin/hugo --gc || exit 1
 docker build . -t pab-web
 docker rm -f pab-web
 docker run -d --name pab-web pab-web

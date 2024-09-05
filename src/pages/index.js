a996a3574366ea80ea317d089e0ef997509f4f45
Pablo Pérez-Aradros <pabloperezaradros@gmail.com>
2024-09-05 15:02:17 +0000

    chore: Update link to cursos section in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs">
+            to="/docs/cursos">
             Cursos 
           </Link>
           <Link

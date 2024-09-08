9596bdbcd38a036e9648c508e70ff4664013393f
pabpereza <pabloperezaradros@gmail.com>
2024-09-08 20:20:58 +0200

    Refactor LatestVideos component to display truncated titles and descriptions

--- src/components/LatestVideos/index.js
+++ src/components/LatestVideos/index.js
@@ -5,24 +5,25 @@ import videos from '../../files/videos.json';
 
 
 const VideoList = videos.map(video => (
-  console.log(video),
   {
   videoid: video.id.videoId,
-  title: video.snippet.title,
+  title: video.snippet.title.substring(0, 100),
+  description: video.snippet.description.substring(0, 100),
 }));
 
 // Let just 3 videos
 VideoList.length = 3;
 
-function Video({videoid, title }) {
+function Video({videoid, title,description }) {
   return (
     <div className={clsx('col col--4')}>
         <a href={`https://youtu.be/${videoid}`} target='_blank'>
           <div className="text--center featureImg">
-              <img width='80%' src={`https://img.youtube.com/vi/${videoid}/maxresdefault.jpg`} />
+              <img width='90%' src={`https://img.youtube.com/vi/${videoid}/maxresdefault.jpg`} />
           </div>
           <div className="text--center padding-horiz--md">
-            <Heading as="h3" >{title}</Heading>
+            <Heading as="h3" >{title}...</Heading>
+            <p>{description}...</p>
           </div>
         </a>
     </div>

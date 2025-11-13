9ea382a459524ba60401fd645080e487361b0412
pabpereza <pabloperezaradros@gmail.com>
2025-11-13 01:04:59 +0100

    feat: Update content graph with new DevOps and Vim courses, enhance NotFound page redirection, and fix 404.html keydown event listener

--- src/theme/NotFound/index.js
+++ src/theme/NotFound/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import NotFound from '@theme-original/NotFound';
 import Layout from '@theme/Layout';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function NotFoundWrapper(props) {
+  const target = useBaseUrl('/404.html');
 
-  window.location.replace('/404.html');
-
+  return (
+    <BrowserOnly fallback={<div>Cargando…</div>}>
+      {() => {
+        // Esto corre solo en el navegador
+        window.location.replace(target);
+        return null;
+      }}
+    </BrowserOnly>
+  );
 }
